0180634fc0fd8bfeb8b9ad10a2495766b9042154
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-01-20 21:49:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ function kup(event){
 	switch(event.keyCode){
 		case 27:
 			history.go(-1);
+			break;
 		case 66:
 			window.location="/yblui-b";
-    case 68:
+			break;
+   		 case 68:
 			window.location="/yblui-d";
+			break;
 	}
 }
