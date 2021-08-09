65e8e973b89bc331a05de108ac5dca0c086a782c
y.b.l** <69856522+yblui@users.noreply.github.com>
2021-08-09 23:07:27 +0800

    Changed 3 Files.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-function kup(event){
-	switch(event.keyCode){
+function kup(event) {
+	switch (event.keyCode) {
 		case 27:
 			history.go(-1);
 			break;
 		case 66:
-			window.location="/yblui-b";
+			window.location = "/yblui-b";
 			break;
-   		case 68:
-			window.location="/yblui-d";
+		case 68:
+			window.location = "/yblui-d";
 			break;
 		case 69:
-			window.location="/yblui-e";
+			window.location = "/yblui-e";
+			break;
+		case 70:
+			window.location = "/yblui-f";
 			break;
 	}
 }
